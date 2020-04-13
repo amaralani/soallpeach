440cfdb48ff7ae5bf9ebf574007ca155cd293a8e
Amir Maralani <amir.maralani@gmail.com>
2020-04-13 18:18:20 +0430

    Remove unused date

--- prime/Main.java
+++ prime/Main.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) throws IOException {

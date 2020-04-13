809981c3101c726e950188204ae6f940c9d51508
Amir Maralani <amir.maralani@gmail.com>
2020-04-13 17:06:49 +0430

    Use stream API

--- prime/Main.java
+++ prime/Main.java
@@ -4,20 +4,16 @@ import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String content = new String(Files.readAllBytes(Paths.get(args[0])));
-        String[] lines = content.split(System.getProperty("line.separator"));
-        for (String line : lines) {
+        Files.lines(Paths.get(args[0])).forEach(line -> {
             if (isPrime(Integer.parseInt(line)))
                 System.out.println("1");
             else
                 System.out.println("0");
-        }
+        });
     }
 
     private static boolean isPrime(int num) {
-        if (num > 2 && num % 2 == 0) {
-            return false;
-        }
+        if (num < 3) return true;
         int top = (int) Math.sqrt(num) + 1;
         for (int i = 3; i < top; i += 2) {
             if (num % i == 0) {

e1e859d8987e86fbcefca1bad4c019ec87cb0e3a
Amir Maralani <amir.maralani@gmail.com>
2020-04-14 17:13:22 +0430

    Return string instead of boolean

--- prime/Main.java
+++ prime/Main.java
@@ -5,25 +5,22 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) throws IOException {
         StringBuilder stringBuilder = new StringBuilder();
-        Files.lines(Paths.get(args[0])).forEach(line -> {
-            if (isPrime(Integer.parseInt(line)))
-                stringBuilder.append("1");
-            else
-                stringBuilder.append("0");
-            stringBuilder.append("\n");
-        });
-        stringBuilder.deleteCharAt(stringBuilder.lastIndexOf("\n"));
+        Files.lines(Paths.get(args[0])).forEach(line -> stringBuilder.append(isPrime(Integer.parseInt(line))));
         System.out.println(stringBuilder.toString());
     }
 
-    private static boolean isPrime(int num) {
-        if (num < 3) return true;
+    private static String isPrime(int num) {
+        if (num < 3) return "1\n";
+        if(num % 2 == 0) return "0\n";
+        if(num % 3 == 0) return "0\n";
+        if(num % 5 == 0) return "0\n";
+        if(num % 7 == 0) return "0\n";
         int top = (int) Math.sqrt(num) + 1;
         for (int i = 3; i < top; i += 2) {
             if (num % i == 0) {
-                return false;
+                return "0\n";
             }
         }
-        return true;
+        return "1";
     }
 }

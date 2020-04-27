7d7f0e4d444c70b72b6f74a3274671b317033426
Amir Maralani <amir.maralani@gmail.com>
2020-04-27 18:15:36 +0430

    Cleanup + fix count bug

--- countme/src/main/java/ir/maralani/ServerHandler.java
+++ countme/src/main/java/ir/maralani/ServerHandler.java
@@ -4,56 +4,50 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 import org.apache.commons.lang3.StringUtils;
 
-public class ServerHandler extends SimpleChannelInboundHandler<Object> {
-    private int count = 0;
+public class ServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+    private static int count = 0;
 
     @Override
-    public void channelRead0(ChannelHandlerContext ctx, Object msg) {
-        FullHttpRequest request = (FullHttpRequest) msg;
+    public void channelRead0(ChannelHandlerContext ctx, FullHttpRequest request) {
         if (request.uri().equals("/") && request.method().equals(HttpMethod.POST)) {
             String stringBody = request.content().toString(CharsetUtil.UTF_8);
             count += Integer.parseInt(StringUtils.trim(stringBody));
-            writeResponse(ctx, request, Unpooled.EMPTY_BUFFER, "text/html", String.valueOf(String.valueOf(count).length()));
+            writeResponse(ctx, Unpooled.EMPTY_BUFFER);
             return;
         }
 
         if (request.uri().equals("/count") && request.method().equals(HttpMethod.GET)) {
             ByteBuf responseContent = Unpooled.copiedBuffer(String.valueOf(count), CharsetUtil.UTF_8);
-            writeResponse(ctx, request, responseContent, "text/html", String.valueOf(String.valueOf(count).length()));
+            writeResponse(ctx, responseContent);
             return;
         }
 
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND, Unpooled.EMPTY_BUFFER, false);
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND,
+                Unpooled.EMPTY_BUFFER, false);
         ctx.write(response).addListener(ChannelFutureListener.CLOSE);
-
     }
 
-    private void writeResponse(ChannelHandlerContext ctx, HttpRequest request, ByteBuf buf, CharSequence contentType, CharSequence contentLength) {
-        // Decide whether to close the connection or not.
-        boolean keepAlive = HttpUtil.isKeepAlive(request);
-        // Build the response object.
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, buf, false);
+    private void writeResponse(ChannelHandlerContext ctx, ByteBuf buf) {
+        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, buf,
+                false);
         HttpHeaders headers = response.headers();
-        headers.set(HttpHeaderNames.CONTENT_TYPE, contentType);
+        headers.set(HttpHeaderNames.CONTENT_TYPE, "text/html");
 
-        // Close the non-keep-alive connection after the write operation is
-        // done.
-            ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+        ctx.write(response).addListener(ChannelFutureListener.CLOSE);
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         ctx.close();
     }
 
     @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+    public void channelReadComplete(ChannelHandlerContext ctx) {
         ctx.flush();
     }
 }

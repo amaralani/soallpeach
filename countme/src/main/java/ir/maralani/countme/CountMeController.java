e73a0a9d1b23d5489affd840ef2dd8b155df08ca
Amir Maralani <amir.maralani@gmail.com>
2020-04-27 12:44:58 +0430

    Fixed body retrieval for POST

--- countme/src/main/java/ir/maralani/countme/CountMeController.java
+++ countme/src/main/java/ir/maralani/countme/CountMeController.java
@@ -1,18 +1,20 @@
 package ir.maralani.countme;
 
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CountMeController {
     private int count = 0;
 
-    @PostMapping(value = "/")
-    @ResponseBody
-    public ResponseEntity addNumberToCount(@RequestParam MultiValueMap<String, String> body) {
-        body.keySet().forEach(key -> count += Integer.parseInt(key));
+    @PostMapping
+    public ResponseEntity addNumberToCount(HttpEntity<byte[]> requestEntity) {
+        count += Integer.parseInt(StringUtils.trimAllWhitespace(new String(requestEntity.getBody())));
         return ResponseEntity.ok().build();
     }
 

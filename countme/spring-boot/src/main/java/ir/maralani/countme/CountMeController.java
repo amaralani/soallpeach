386214c82a561bd8c53ce99f6ff980e61458e513
Amir Maralani <amir.maralani@gmail.com>
2020-04-28 15:15:14 +0430

    Vertex

--- countme/spring-boot/src/main/java/ir/maralani/countme/CountMeController.java
+++ countme/spring-boot/src/main/java/ir/maralani/countme/CountMeController.java
@@ -8,18 +8,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class CountMeController {
-    private int count = 0;
+    private List<String> list = new ArrayList<>();
 
     @PostMapping
     public ResponseEntity addNumberToCount(HttpEntity<byte[]> requestEntity) {
-        count += Integer.parseInt(StringUtils.trimAllWhitespace(new String(requestEntity.getBody())));
+        add(new String(requestEntity.getBody()));
         return ResponseEntity.ok().build();
     }
 
     @RequestMapping(value = "/count", method = RequestMethod.GET)
     public ResponseEntity getCount() {
-        return ResponseEntity.ok(count);
+        return ResponseEntity.ok(list.stream().mapToLong(value -> Integer.parseInt(value)).sum());
+    }
+
+    @RequestMapping(value = "/reset", method = RequestMethod.GET)
+    public ResponseEntity resetCount() {
+        list.clear();
+        return ResponseEntity.ok().build();
+    }
+
+    private synchronized void add(String value) {
+        list.add(StringUtils.trimAllWhitespace(value));
     }
 }
